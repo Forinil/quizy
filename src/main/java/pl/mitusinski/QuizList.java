0b555ccaed3bd60666b1e6e2b7591858192dc99d
Łukasz <l.mitusinski@gmail.com>
2017-12-11 19:26:08 +0100

    Editable quiz language

--- src/main/java/pl/mitusinski/QuizList.java
+++ src/main/java/pl/mitusinski/QuizList.java
@@ -1,34 +1,38 @@
 package pl.mitusinski;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by Łukasz on 18.05.2017.
+ * github.com/lmitu
  */
 @XmlAccessorType(XmlAccessType.NONE)
 @XmlRootElement(name = "quizlist")
-public class QuizList {
-
-    public QuizList() {
-    }
-
-    public QuizList(List<Quiz> quizList) {
-        this.quizList = quizList;
-    }
+class QuizList {
 
     @XmlElement(name = "quiz")
     private List<Quiz> quizList = new ArrayList<>();
 
-    public List<Quiz> getQuizList() {
+    QuizList() {
+    }
+
+    QuizList(List<Quiz> quizList) {
+        this.quizList = quizList;
+    }
+
+    List<Quiz> getQuizList() {
         return quizList;
+    }
+
+    Optional<Quiz> getElementById(String id) {
+        return quizList.stream()
+                .filter(q -> q.getId().equals(id))
+                .findFirst();
     }
 }
